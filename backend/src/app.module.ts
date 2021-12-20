13b633761bc8441bc7283f7a50e27cd645ae69c7
Joann ACHER <jacher@e2r9p4.clusters.42paris.fr>
2021-12-20 15:43:50 +0100

    db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './entities/user.entity';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { UserHttpModule } from './user-http/user-http.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+    UserHttpModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
---
+++ backend/src/user/user.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn, OneToMany} from "typeorm";
+
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+  
+    @Column()
+    lastName: string;
+  
+    @Column({ default: true })
+    isActive: boolean;
+
+}
