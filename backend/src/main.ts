1e1dc21e785373ad56b2a06fccfdc732ed016083
Tom Vacherat <tvacherat@gmail.com>
2022-01-08 12:34:28 +0100

    HTTPS set -- auth.todo added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import * as fs from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	// HTTPS application params
+	const httpsOptions = {
+		key: fs.readFileSync('./secrets/private-key.pem'),
+		cert: fs.readFileSync('./secrets/public-certificate.pem'),
+	};
+
+	// Application creation and configuration
+	const app = await NestFactory.create(AppModule, {
+		httpsOptions,
+	});
 
 	// cookieParser middleware as global for parsing cookies
 	app.use(cookieParser());
