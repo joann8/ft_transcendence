7ea7d35a61fc0087acabdab098d8f82c990fac66
Tom Vacherat <tvacherat@gmail.com>
2022-01-17 19:06:42 +0100

    front in progress

--- front/src/components/Login/Login.tsx
+++ front/src/components/Login/Login.tsx
@@ -1,39 +1,38 @@
 import { Send } from "@mui/icons-material";
 import { Grid, Button, Typography, Paper } from "@mui/material";
 import React, { Fragment } from "react";
+import background from "./bg.jpg";
 
 export default function Login() {
   return (
-    <Fragment>
-      <Grid
-        container
-        alignItems="center"
-        justifyContent="center"
-        direction={"column"}
-        margin={6}
-      >
-        <Grid item>
-          <Typography
-            variant="h3"
-            align="center"
-            fontFamily={"Arial Black"}
-            marginBottom={4}
-          >
-            TRANSCENDENCE
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Button
-            variant="contained"
-            href="http://127.0.0.1:3001/login/42"
-            size="large"
-            endIcon={<Send />}
-          >
-            Login with 42
-          </Button>
-        </Grid>
-        <Paper></Paper>
+    <Grid
+      container
+      alignItems="center"
+      justifyContent="center"
+      direction={"column"}
+      marginTop={3}
+    >
+      <Grid item>
+        <Typography
+          variant="h3"
+          align="center"
+          fontFamily={"Arial Black"}
+          marginBottom={4}
+        >
+          TRANSCENDENCE
+        </Typography>
       </Grid>
-    </Fragment>
+      <Grid item>
+        <Button
+          variant="contained"
+          href="http://127.0.0.1:3001/login/42"
+          size="large"
+          endIcon={<Send />}
+        >
+          Login with 42
+        </Button>
+      </Grid>
+      <Paper></Paper>
+    </Grid>
   );
 }
