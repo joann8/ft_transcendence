b698ac8832e806d074158ce27275472c39fa3724
tvacherat <tvacherat@gmail.com>
2022-01-18 18:21:08 +0100

    front login in progress

--- front/src/components/Login/Login.tsx
+++ front/src/components/Login/Login.tsx
@@ -1,10 +1,35 @@
 import { Send } from "@mui/icons-material";
-import { Grid, Button, Typography, Paper } from "@mui/material";
-import React, { Fragment } from "react";
-import background from "./bg.jpg";
+import { Button, Grid, Typography } from "@mui/material";
+import React from "react";
+import LoginMask from "./LoginMask";
 
 export default function Login() {
   return (
+    <div>
+      <LoginMask />
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        direction={"column"}
+        marginTop={15}
+        position={"absolute"}
+      >
+        <Grid item>
+          <Button
+            variant="contained"
+            href="http://127.0.0.1:3001/login/42"
+            size="large"
+            endIcon={<Send />}
+          >
+            Login with 42
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
+
+  /*return (
     <Grid
       container
       alignItems="center"
@@ -34,5 +59,5 @@ export default function Login() {
       </Grid>
       <Paper></Paper>
     </Grid>
-  );
+  );*/
 }
