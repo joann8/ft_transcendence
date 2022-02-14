a389c2dc3e2a7a8f043de36481ec727f4270dad1
tvacherat <tvacherat@gmail.com>
2022-02-14 11:42:43 +0100

    premerge

--- front/src/ApiCalls/var.tsx
+++ front/src/ApiCalls/var.tsx
@@ -3,6 +3,4 @@ export const api_url = "http://127.0.0.1:3001";
 export const api_req_init: RequestInit = {
   method: "GET",
   credentials: "include",
- // referrerPolicy: "same-origin",
-  //   signal ?
 };
